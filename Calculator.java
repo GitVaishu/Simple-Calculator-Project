f4518a9b3e3add6a286e442f5b8f84cd4c434b05
GitVaishu <vaishnavipandule1412@gmail.com>
2026-02-02 12:20:31 +0530

    Multiplication added

--- Calculator.java
+++ Calculator.java
@@ -15,7 +15,13 @@ public class Calculator {
 
         int result;
         //create new function for each operation
-    
+        Multiplication multiplication = new Multiplication();
+        result = multiplication.multiply(num1, num2);
         System.out.println(result);
     }
+    public class Multiplication {
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
 }
