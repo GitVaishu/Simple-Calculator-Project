fa650c5e776acb1000e0d60c4f813e527636b0d8
GitVaishu <vaishnavipandule1412@gmail.com>
2026-02-02 11:58:42 +0530

    Input var handled

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,21 @@
 package calculator;
+import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Calculator Project Template");
         System.out.println("Members will implement operations separately.");
+        int num1;
+        int num2;
+        System.out.println("Enter two numbers to perform calculations:");
+        // Input handling can be added here
+        num1 = sc.nextInt();
+        num2 = sc.nextInt();
+
+        int result;
+        //create new function for each operation
+    
+        System.out.println(result);
     }
 }
