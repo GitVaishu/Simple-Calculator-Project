8353cffbea0aa836f031ed67f4cab5f424f6ef98
GitVaishu <vaishnavipandule1412@gmail.com>
2026-02-02 11:52:47 +0530

    Base file

---
+++ Calculator.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Calculator Project Template");
+        System.out.println("Members will implement operations separately.");
+    }
+}
